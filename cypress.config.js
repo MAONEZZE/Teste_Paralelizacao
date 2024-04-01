b0c1b43664b8de08e8a28d9806a4133c6db35f6a
ruan.souza <ruan.souza@ndd.tech>
2024-04-01 18:41:37 -0300

    wkflow2

--- cypress.config.js
+++ cypress.config.js
@@ -5,7 +5,7 @@ const cypressSplit = require('cypress-split')
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
-      cypressSplit(on, config)
+      cypressSplit(on, config) //aqui está usando o cypress split
       // IMPORTANT: return the config object
       return config
     },
