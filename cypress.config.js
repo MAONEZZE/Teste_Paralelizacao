dc311df2860f405e03d34c20cd6457f3fd4f812f
ruan.souza <ruan.souza@ndd.tech>
2024-04-01 18:15:54 -0300

    workflow

---
+++ cypress.config.js
@@ -0,0 +1,13 @@
+const { defineConfig } = require("cypress");
+
+const cypressSplit = require('cypress-split')
+
+module.exports = defineConfig({
+  e2e: {
+    setupNodeEvents(on, config) {
+      cypressSplit(on, config)
+      // IMPORTANT: return the config object
+      return config
+    },
+  },
+});
